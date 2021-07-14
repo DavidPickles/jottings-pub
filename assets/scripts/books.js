a9c2561cf9fa82ea326d3b8c2d13e1c903504acc
David Pickles <davidkpickles@gmail.com>
2021-07-14 10:33:27 +0100

    new book record

--- assets/scripts/books.js
+++ assets/scripts/books.js
@@ -1,8 +1,21 @@
 import * as jquery from './lib/jquery-3.6.0.min.js'
 
-export default ({ratingsSelector}) => {
+const ratingsSelector = '.rating-stars'
+const sortKeySelector = '[name="book-sort-key"]'
+const sortDirectionSelector = '[name="book-sort-direction"]'
+const bookSelector = '.book'
+const bookListSelector = '#books'
+
+let sortKey
+let sortDirection
+
+export default () => {
     $( document ).ready(  () => {
         $(ratingsSelector).each( (i,el) => el.innerHTML = toStars( el.dataset.ratingValue) )
+        sortKey = $(sortKeySelector + ':checked').val()
+        sortDirection = $(sortDirectionSelector + ':checked').val()
+        $(sortKeySelector).change( ev => setSortKey(ev.currentTarget.value) )
+        $(sortDirectionSelector).change( ev => setSortDirection(ev.currentTarget.value) )
     })
 }
 
@@ -13,4 +26,30 @@ function toStars(val) {
     const half = stars("fas fa-star-half-alt", +val%2)
     const empty = stars("far fa-star", 5 - val/2)
     return full + half + empty
+}
+
+function setSortDirection(direction) {
+    sortDirection = direction
+    $(bookListSelector).html( getSortedBooks(sortKey, sortDirection) )
+}
+
+function setSortKey(key) {
+    sortKey = key
+    $(bookListSelector).html( getSortedBooks(sortKey, sortDirection) )
+}
+
+function getSortedBooks(sortKey, sortDirection) {
+    const numeric = (orientation, a, b) => orientation * (parseInt(a) - parseInt(b))
+    const alpha = (orientation, a, b) => orientation * ( a.toLowerCase().localeCompare(b.toLowerCase()) )
+    const compOrientation = sortDirection === 'ascending' ? 1 : -1
+    const sortedBooks = $(bookSelector).sort( (el1, el2) => {
+        const a = el1.dataset[sortKey]
+        const b = el2.dataset[sortKey]
+        if (sortKey === 'ratingValue') {
+            return numeric(compOrientation, a, b)
+        } else {
+            return alpha(compOrientation, a, b)
+        }
+    })
+    return sortedBooks
 }
